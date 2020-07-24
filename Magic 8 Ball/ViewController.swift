980c0ce94bdb57feb7c2145b414ec1b17baedf15
Robert Mutch <rrmutch@gmail.com>
2020-07-24 16:01:37 -0700

    First commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Robert Mutch on 07/20/2020.
+//  Copyright © Robert Mutch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+
+
+}
+
